1d720318c6adb8fed99e4ab231512ef0a15f8a36
Eray TURAN <turanerayy@gmail.com>
2024-07-26 17:04:22 +0300

    Added Api for WhoWeAreDetail-Services and HomePage's ProductList  edited

--- RealEstate_Dapper_Api/RealEstate_Dapper_UI/Dtos/ProductDtos/ResultProductDtos.cs
+++ RealEstate_Dapper_Api/RealEstate_Dapper_UI/Dtos/ProductDtos/ResultProductDtos.cs
@@ -2,11 +2,14 @@
 {
     public class ResultProductDtos
     {
-            public int productID { get; set; }
+            public int productID { get; set; }  
             public string title { get; set; }
             public decimal price { get; set; }
             public string city { get; set; }
             public string district { get; set; }
-            public string categoryName { get; set; }  
+            public string categoryName { get; set; }
+            public string coverImage { get; set; }
+            public string type { get; set; }
+            public string address { get; set; }
     }
 }
