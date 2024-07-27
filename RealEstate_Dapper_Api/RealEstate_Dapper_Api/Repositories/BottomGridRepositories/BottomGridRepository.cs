c4c1ff48d8c7554458a58e1d784f1bd942dea236
Eray TURAN <turanerayy@gmail.com>
2024-07-27 15:48:10 +0300

    Added api and consume operations for BottomGrid and PopularLocations

---
+++ RealEstate_Dapper_Api/RealEstate_Dapper_Api/Repositories/BottomGridRepositories/BottomGridRepository.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
+using RealEstate_Dapper_Api.Dtos.ProductDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.BottomGridRepositories
+{
+    public class BottomGridRepository : IBottomGridRepository
+    {
+        private readonly Context _context;
+
+        public BottomGridRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void CreateButtomGrid(CreateBottomGridDto createBottomGridDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteButtomGrid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultBottomGridDto>> GetAllButtomGridAsync()
+        {
+            string query = "Select * From BottomGrid";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultBottomGridDto>(query);
+                return values.ToList();
+            }
+        }
+        public Task<GetBottomGridDto> GetButtomGrid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateButtomGrid(UpdateBottomGridDto updateBottomGridDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/RealEstate_Dapper_Api/Repositories/BottomGridRepositories/IBottomGridRepository.cs
@@ -0,0 +1,13 @@
+﻿using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.BottomGridRepositories
+{
+    public interface IBottomGridRepository
+    {
+        Task<List<ResultBottomGridDto>> GetAllButtomGridAsync();
+        void CreateButtomGrid(CreateBottomGridDto createBottomGridDto);
+        void DeleteButtomGrid(int id);  
+        void UpdateButtomGrid(UpdateBottomGridDto updateBottomGridDto);
+        Task<GetBottomGridDto> GetButtomGrid(int id);
+    }
+}
