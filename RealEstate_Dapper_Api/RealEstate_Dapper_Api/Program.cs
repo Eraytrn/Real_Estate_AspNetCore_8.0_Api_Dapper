9eda8d52bff5a7cf2039fa82f44a74488baa735e
Eray TURAN <turanerayy@gmail.com>
2024-08-08 14:57:13 +0300

    SignalR connection operations completed

--- RealEstate_Dapper_Api/RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/RealEstate_Dapper_Api/Program.cs
@@ -39,6 +39,8 @@ builder.Services.AddCors(opt =>
                .AllowCredentials();
     });
 });
+
+builder.Services.AddHttpClient();
 builder.Services.AddSignalR();
 
 builder.Services.AddControllers();
