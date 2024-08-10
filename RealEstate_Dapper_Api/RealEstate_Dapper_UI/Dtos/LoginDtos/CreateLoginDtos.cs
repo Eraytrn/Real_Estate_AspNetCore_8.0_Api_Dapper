a79e83b8343a46556a5929b1325ce14ffc0037f8
Eray TURAN <turanerayy@gmail.com>
2024-08-10 17:38:38 +0300

    ProductAdvertsListByEmployee and SigIn with Jwt api operations made

---
+++ RealEstate_Dapper_Api/RealEstate_Dapper_UI/Dtos/LoginDtos/CreateLoginDtos.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.LoginDtos
+{
+    public class CreateLoginDtos
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
